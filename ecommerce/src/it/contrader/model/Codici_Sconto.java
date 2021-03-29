5f20359f614ef8758395276f539863fde1b396f6
Nicolò Paolangeli <78351849+NicoloPaolangeli@users.noreply.github.com>
2021-03-29 17:43:32 +0200

    Update Codici_Sconto.java

--- ecommerce/src/it/contrader/model/Codici_Sconto.java
+++ ecommerce/src/it/contrader/model/Codici_Sconto.java
@@ -7,23 +7,18 @@ public class Codici_Sconto {
 	/**
 	 * Qui sotto definisco gli attributi di User. 
 	 */
-	private int id_ordini;
+	private int id_codice_sconto;
 
-	private int id_prodotti;
+	private int id_ordini;
 	
-	private int quantita;
+	private int sconto_percentuale;
 	
-	private int id_clienti;
+	private String data_emissione;
 	
-	private int iva;
+	private Boolean riscossione;
 	
-	private int prezzo_totale;
+	private String data_scadenza;
 	
-	private String data_ordine;
-	
-	private String indirizzo_spedizione;
-	
-	private String modalita_pagamento;
 	
 	
 
@@ -34,35 +29,30 @@ public class Codici_Sconto {
 		
 	}
 
-	public Codici_Sconto (int id_prodotti, int quantita, int id_clienti, int iva, int prezzo_totale, String data_ordine, String indirizzo_spedizione, String modalita_pagamento) {
-		this.id_prodotti = id_prodotti;
-		this.quantita = quantita;
-		this.id_clienti = id_clienti;
-		this.iva = iva;
-		this.prezzo_totale = prezzo_totale;
-		this.data_ordine = data_ordine;
-		this.indirizzo_spedizione = indirizzo_spedizione;
-		this.modalita_pagamento = modalita_pagamento;
-	}
-
-	public Codici_Sconto (int id_ordini, int id_prodotti, int quantita, int id_clienti, int iva, int prezzo_totale, String data_ordine, String indirizzo_spedizione, String modalita_pagamento) {
+	public Codici_Sconto (int id_ordini, int sconto_percentuale, String data_emissione, Boolean riscossione, String data_scadenza) {
 		this.id_ordini = id_ordini;
-		this.id_prodotti = id_prodotti;
-		this.quantita = quantita;
-		this.id_clienti = id_clienti;
-		this.iva = iva;
-		this.prezzo_totale = prezzo_totale;
-		this.data_ordine = data_ordine;
-		this.indirizzo_spedizione = indirizzo_spedizione;
-		this.modalita_pagamento = modalita_pagamento;
+		this.sconto_percentuale = sconto_percentuale;
+		this.data_emissione = data_emissione;
+		this.riscossione = riscossione;
+		this.data_scadenza = data_scadenza;
 	}
 
-	/**
-	 * Getter e Setter: servono alle altre classi a recuperare e modificare gli attributi di User
-	 */
-	
-	
+	public Codici_Sconto (int id_codice_sconto, int id_ordini, int sconto_percentuale, String data_emissione, Boolean riscossione, String data_scadenza) {
+		this.id_codice_sconto = id_codice_sconto;
+		this.id_ordini = id_ordini;
+		this.sconto_percentuale = sconto_percentuale;
+		this.data_emissione = data_emissione;
+		this.riscossione = riscossione;
+		this.data_scadenza = data_scadenza;
+	}
 
+	public int getId_codice_sconto() {
+		return id_codice_sconto;
+	}
+
+	public void setId_codice_sconto(int id_codice_sconto) {
+		this.id_codice_sconto = id_codice_sconto;
+	}
 
 	public int getId_ordini() {
 		return id_ordini;
@@ -72,71 +62,46 @@ public class Codici_Sconto {
 		this.id_ordini = id_ordini;
 	}
 
-	public int getId_prodotti() {
-		return id_prodotti;
+	public int getSconto_percentuale() {
+		return sconto_percentuale;
 	}
 
-	public void setId_prodotti(int id_prodotti) {
-		this.id_prodotti = id_prodotti;
+	public void setSconto_percentuale(int sconto_percentuale) {
+		this.sconto_percentuale = sconto_percentuale;
 	}
 
-	public int getQuantita() {
-		return quantita;
+	public String getData_emissione() {
+		return data_emissione;
 	}
 
-	public void setQuantita(int quantita) {
-		this.quantita = quantita;
+	public void setData_emissione(String data_emissione) {
+		this.data_emissione = data_emissione;
 	}
 
-	public int getId_clienti() {
-		return id_clienti;
+	public Boolean getRiscossione() {
+		return riscossione;
 	}
 
-	public void setId_clienti(int id_clienti) {
-		this.id_clienti = id_clienti;
+	public void setRiscossione(Boolean riscossione) {
+		this.riscossione = riscossione;
 	}
 
-	public int getIva() {
-		return iva;
+	public String getData_scadenza() {
+		return data_scadenza;
 	}
 
-	public void setIva(int iva) {
-		this.iva = iva;
+	public void setData_scadenza(String data_scadenza) {
+		this.data_scadenza = data_scadenza;
 	}
 
-	public int getPrezzo_totale() {
-		return prezzo_totale;
+	@Override
+	public String toString() {
+		return "Codici_Sconto [id_codice_sconto=" + id_codice_sconto + ", id_ordini=" + id_ordini
+				+ ", sconto_percentuale=" + sconto_percentuale + ", data_emissione=" + data_emissione + ", riscossione="
+				+ riscossione + ", data_scadenza=" + data_scadenza + "]";
 	}
 
-	public void setPrezzo_totale(int prezzo_totale) {
-		this.prezzo_totale = prezzo_totale;
-	}
 
-	public String getData_ordine() {
-		return data_ordine;
-	}
-
-	public void setData_ordine(String data_ordine) {
-		this.data_ordine = data_ordine;
-	}
-
-	public String getIndirizzo_spedizione() {
-		return indirizzo_spedizione;
-	}
-
-	public void setIndirizzo_spedizione(String indirizzo_spedizione) {
-		this.indirizzo_spedizione = indirizzo_spedizione;
-	}
-
-	public String getModalita_pagamento() {
-		return modalita_pagamento;
-	}
-
-	public void setModalita_pagamento(String modalita_pagamento) {
-		this.modalita_pagamento = modalita_pagamento;
-	}
-
-	
 
 	@Override
 	public boolean equals(Object obj) {
@@ -147,49 +112,34 @@ public class Codici_Sconto {
 		if (getClass() != obj.getClass())
 			return false;
 		Codici_Sconto other = (Codici_Sconto) obj;
-		if (data_ordine == null) {
-			if (other.data_ordine != null)
+		if (data_emissione == null) {
+			if (other.data_emissione != null)
 				return false;
-		} else if (!data_ordine.equals(other.data_ordine))
+		} else if (!data_emissione.equals(other.data_emissione))
 			return false;
-		if (id_clienti != other.id_clienti)
+		if (data_scadenza == null) {
+			if (other.data_scadenza != null)
+				return false;
+		} else if (!data_scadenza.equals(other.data_scadenza))
+			return false;
+		if (id_codice_sconto != other.id_codice_sconto)
 			return false;
 		if (id_ordini != other.id_ordini)
 			return false;
-		if (id_prodotti != other.id_prodotti)
-			return false;
-		if (indirizzo_spedizione == null) {
-			if (other.indirizzo_spedizione != null)
+		if (riscossione == null) {
+			if (other.riscossione != null)
 				return false;
-		} else if (!indirizzo_spedizione.equals(other.indirizzo_spedizione))
+		} else if (!riscossione.equals(other.riscossione))
 			return false;
-		if (iva != other.iva)
-			return false;
-		if (modalita_pagamento == null) {
-			if (other.modalita_pagamento != null)
-				return false;
-		} else if (!modalita_pagamento.equals(other.modalita_pagamento))
-			return false;
-		if (prezzo_totale != other.prezzo_totale)
-			return false;
-		if (quantita != other.quantita)
+		if (sconto_percentuale != other.sconto_percentuale)
 			return false;
 		return true;
 	}
 
-	@Override
-	public String toString() {
-		return "Ordini [id_ordini=" + id_ordini + ", id_prodotti=" + id_prodotti + ", quantita=" + quantita
-				+ ", id_clienti=" + id_clienti + ", iva=" + iva + ", prezzo_totale=" + prezzo_totale + ", data_ordine="
-				+ data_ordine + ", indirizzo_spedizione=" + indirizzo_spedizione + ", modalita_pagamento="
-				+ modalita_pagamento + ", getId_ordini()=" + getId_ordini() + ", getId_prodotti()=" + getId_prodotti()
-				+ ", getQuantita()=" + getQuantita() + ", getId_clienti()=" + getId_clienti() + ", getIva()=" + getIva()
-				+ ", getPrezzo_totale()=" + getPrezzo_totale() + ", getData_ordine()=" + getData_ordine()
-				+ ", getIndirizzo_spedizione()=" + getIndirizzo_spedizione() + ", getModalita_pagamento()="
-				+ getModalita_pagamento() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
-				+ ", toString()=" + super.toString() + "]";
-	}
+	
+
 	
 	
 	
+	//end	
 }
